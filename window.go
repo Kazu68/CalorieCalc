26a0fdf6ffb7fe05841f516b1239ed6d49ddc815
Kazu68 <oka.nobu5rh@gmail.com>
2020-12-20 12:22:02 +0900

    window add

--- window.go
+++ window.go
@@ -1,45 +1,156 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
-	"golang.org/x/exp/shiny/driver"
-	"golang.org/x/exp/shiny/screen"
-	"golang.org/x/mobile/event/key"
-	"golang.org/x/mobile/event/lifecycle"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
 
-func main() {
-	driver.Main(func(s screen.Screen) {
-		w, err := s.NewWindow(&screen.NewWindowOptions{
-			Title:  "test",
-			Height: 500,
-			Width:  600,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer w.Release()
-
-		for {
-			e := w.NextEvent() //イベントを受け取る
-
-			switch e := e.(type) {
-			case lifecycle.Event:
-				//fmt.Printf("lifecycle.Event%v¥", e)
-				if e.To == lifecycle.StageDead {
-					return
-				}
-
-			case key.Event:
-				//fmt.Printf("key.Event%v¥", e)
-				if e.Code == key.CodeEscape {
-					return
-				}
-
-			case error:
-				log.Print(e)
-			}
-		}
-	})
+type MyMainWindow struct {
+	*walk.MainWindow
+	male        *walk.RadioButton
+	edit        *walk.TextEdit
+	female      *walk.RadioButton
+	genderValue int // Gender
+	activeValue int // activelevel
+	heightBox  *walk.LineEdit
+	weightdBox *walk.LineEdit
+	ageBox *walk.LineEdit
 }
+
+var heightValue string
+var weightValue string
+var ageValue int
+var maleValue int
+var femaleValue int
+
+// Basal metabolism
+var Bmr int
+
+func main() {
+	mw := &MyMainWindow{}
+	mw.genderValue = 0
+
+	MW := MainWindow{
+		AssignTo: &mw.MainWindow,
+		Title:    "Info input",
+		MinSize:  Size{300, 200},
+		Size:     Size{300, 300},
+		Layout:   VBox{},
+		Children: []Widget{
+			RadioButtonGroupBox{
+				//				AssignTo: &mw.rdgb1,
+				Title:  "Gender",
+				Layout: HBox{},
+				Buttons: []RadioButton{
+					RadioButton{
+						AssignTo:  &mw.male,
+						Text:      "Male",
+						Value:     10,
+						OnClicked: mw.maleClicked,
+					},
+					RadioButton{
+						AssignTo:  &mw.female,
+						Text:      "Female",
+						Value:     20,
+						OnClicked: mw.femaleClicked,
+					},
+				},
+			},
+
+			RadioButtonGroupBox{
+
+				Title:  "Activity level",
+				Layout: HBox{},
+				Buttons: []RadioButton{
+					RadioButton{
+
+						Text:  "1",
+						Value: 1,
+					},
+					RadioButton{
+
+						Text:  "2",
+						Value: 2,
+					},
+					RadioButton{
+
+						Text:  "3",
+						Value: 3,
+					},
+					RadioButton{
+
+						Text:  "4",
+						Value: 4,
+					},
+					RadioButton{
+
+						Text:  "5",
+						Value: 5,
+					},
+				},
+			},
+			Children: []Widget{
+
+				Label{
+					Text: "身長",
+				},
+				LineEdit{
+					AssignTo: &mw.heightBox,
+					Text:     heightValue,
+				},
+				Label{
+					Text: "体重",
+				},
+				LineEdit{
+					AssignTo: &mw.weightBox,
+					Text:     weightValue,
+				},
+				Label{
+					Text: "年齢",
+				},
+				LineEdit{
+					AssignTo: &mw.ageBox,
+					Text:     ageValue,
+				},
+
+			TextEdit{
+				AssignTo: &mw.edit,
+			},
+			PushButton{
+				Text:      "calculate",
+				OnClicked: mw.pbClicked,
+			},
+		},
+	},
+
+	if _, err := MW.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func (mw *MyMainWindow) maleClicked() {
+		//ハリス・ベネディクト方程式
+		Hbmr := 13.397*weightValue + 4.799*heightValue - 5.677*ageValue + 88.362
+		Bmr = Hbmr - 0.0784*Hbmr
+}
+
+func (mw *MyMainWindow) femaleClicked() {
+	Hbmr := 9.247*weightValue + 3.098*heightValue - 4.33*ageValue + 447.593
+	Bmr = Hbmr - 0.0753*Hbmr
+}
+
+
+/*func (mw *MyMainWindow) pbClicked() {
+	var s string
+	if mw.genderValue == 0 {
+		s = fmt.Sprintf("")
+	} else {
+		s = fmt.Sprintf("INFO : Button %d is selected\r\n", mw.genderValue)
+	}
+	mw.edit.AppendText(s)
+}*/
